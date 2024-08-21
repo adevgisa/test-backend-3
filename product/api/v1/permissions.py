f24ff713a7f9955d874d673a28f7587ff51939d0
user <you@example.com>
2024-08-21 10:53:44 +0300

    change permissions

--- product/api/v1/permissions.py
+++ product/api/v1/permissions.py
@@ -1,18 +1,18 @@
+import pdb
+
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 from users.models import Subscription
 
 
 def make_payment(request):
-    # TODO
-    pass
-
+    pdb.set_trace()
 
 class IsStudentOrIsAdmin(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
-        return obj.author == request.user
+        return request.user.is_authenticated
 
 
 class ReadOnlyOrIsAdmin(BasePermission):
